5ddf49919133308136f5833db59c54ce7627a503
Pavneet <pavanghai@yahoo.com>
2022-08-24 19:31:10 -0700

    Day3

---
+++ python/learning/AngelaYu/D003a_OddEven.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+result = 'even' if number % 2 == 0 else 'odd'
+print(f"This is an {result} number.")
