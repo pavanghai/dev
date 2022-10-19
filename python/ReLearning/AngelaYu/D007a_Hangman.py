53246f56fe2aa1642174b4e01e1aa408d7704a31
Pavneet <pavanghai@yahoo.com>
2022-10-18 21:04:08 -0700

    need finetuning on hangman game

--- python/ReLearning/AngelaYu/D007a_Hangman.py
+++ python/ReLearning/AngelaYu/D007a_Hangman.py
@@ -1,48 +1,44 @@
+from os import system, name
 from random import choice
+from time import sleep
+
 from D007a_Hangman_art import stages, logo
+from D007a_Hangman_word import word_list
 
-"""
-Flow Chart: https://drive.google.com/file/d/1jD5Gw6nSx-dpMhHhUHqsWOkHzOOT7iGG/view?usp=sharing   
-"""
 
-FRUITS = ('apples', 'orange', 'mango', 'pear',
-                'peache', 'apricot', 'grapes', 'kiwi',
-                'watermelon', 'banana')
-# print(logo)
-fruit = "banana".upper() # TODO: replace with choice method --> choice(FRUITS).upper()
-fruit_length = len(fruit)
-fruit_line = list('_' * fruit_length)
-
-print(f"{fruit} - length {fruit_length}")
-print(f"guess the fruit name {fruit_line}\n")
-print(f"{fruit_line}\n") # TODO: convert to string with space in between, this is just for display
-
-game_over = False
+word = choice(word_list).upper()
+display_word = list('_' * len(word))
 life_count = len(stages) - 1
 
+wrong_guess = []
+game_over, lost_life = False # Created switch
+
+print(logo)
 while not game_over:
-    # TODO: print the blank lines of the word 
+    # print(f"{word} - length {len(word)}") # Use for testing
+    print(f"{' '.join(display_word)}\n")
+    if lost_life:
+        print(stages[life_count])
     guess_char = input("Guess a letter: ").upper()
-    if guess_char in fruit:
-        for i in range(len(fruit)):
-            if fruit[i] == guess_char:
-                fruit_line[i] = guess_char
-        print(f"After {fruit_line}")
-        if "_" not in fruit_line:
+    
+    if guess_char in word:
+        for i in range(len(word)):
+            if word[i] == guess_char:
+                display_word[i] = guess_char
+        if "_" not in display_word:
             print("You win...")
             game_over = True
     else:
-        print(f"You guessed {guess_char}, that's not in the word. You lose a life.")
-        print(stages[life_count])
         if life_count > 0:
-            life_count -= 1
+            if guess_char not in wrong_guess:
+                print(f"You guessed {guess_char}, that's not in the word. You lose a life.")
+                life_count -= 1
+                lost_life = True
+            else:
+                print(f"You already guessed {guess_char} try other letter")
+            wrong_guess.append(guess_char)
         else:
             print("You lost the game...")
             game_over = True
-
-# if guess_char in fruit:
-#     print(guess_char)
-#     guess_char_index = fruit.find(guess_char)
-
-# for s in reversed(stages):
-#     print(s)
+    sleep(2)
+    system('cls' if name in ('nt', 'dos') else 'clear')
---
+++ python/ReLearning/AngelaYu/D007a_Hangman_word.py
@@ -0,0 +1,3 @@
+word_list = ('apples', 'orange', 'mango', 'pear',
+          'peache', 'apricot', 'grapes', 'kiwi',
+          'watermelon', 'banana')
