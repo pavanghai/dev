4c9265671a1165c103e2d91d6a4077360c2d3b76
Pavneet <pavanghai@yahoo.com>
2022-10-26 21:22:38 -0700

    Day 9

---
+++ python/ReLearning/AngelaYu/D008_CaesarCiper.py
@@ -0,0 +1,30 @@
+alphabet = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            ]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    elif cipher_direction == "encode":
+        shift_amount *= 1
+    else:
+        raise ("invalid code, please use 'encode' or 'decode' only")
+
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {direction}d result: {end_text}")
+
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
