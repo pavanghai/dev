0c376f01289b1315ed06592d8cb21f7d602dc965
Pavneet <pavanghai@yahoo.com>
2022-11-05 22:58:58 -0700

    guess number game

---
+++ python/ReLearning/AngelaYu/D11_D20/D12_NumberGuessing.py
@@ -0,0 +1,37 @@
+from random import randint
+from sys import exit
+from D12_NumberGuessingArt import logo
+
+answer = randint(1, 100)
+game_continue = True
+# print(f"Guessed number: {answer}")
+
+print(logo)
+print("\nWelcome to the Number Gussing Game!\n"
+      "I'm thinking of a number between 1 and 100.")
+
+level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+if level == 'easy':
+    guess_attempt = 10
+elif level == 'hard':
+    guess_attempt = 5
+else:
+    exit("invalid input, exiting Guess Number Game...")
+
+while game_continue and guess_attempt > 0:
+    print(f"You have {guess_attempt} attempts remaining to guess the number")
+    user_guessed = int(input("Make a guess: "))
+    if user_guessed == answer:
+        print("you guessed it correct...")
+        game_continue = False
+    else:
+        if user_guessed > answer:
+            print("Too high.")
+        else:
+            print("Too low.")
+
+        if guess_attempt != 1:
+            print("Guess again.")
+        guess_attempt -= 1
+
+print("Game over")
