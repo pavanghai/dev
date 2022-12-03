84b71dc6de6c88357210bf78b8fccfc6b997af2c
Pavneet <pavanghai@yahoo.com>
2022-12-02 23:08:29 -0700

    Day17 Start

--- python/ReLearning/AngelaYu/D15/D17_quiz_brain.py
+++ python/ReLearning/AngelaYu/D15/D17_quiz_brain.py
@@ -1,19 +1,19 @@
 class QuizBrain:
 
-    def __int__(self, q_list):
+    def __init__(self, q_list):
         self.q_num = 0
         self.q_list = q_list
         self.score = 0
 
     def next_question(self):
-        current_question = self.q_list[self.q_num]
-        input(f"Q.{self.q_num}: {current_question.text} (True/False)?: ")
+        # current_question = self.q_list[self.q_num]
+        # input(f"Q.{self.q_num}: {current_question.text} (True/False)?: ")
 
-        # u_answer = input(f"Q.{self.q_num + 1}: {self.q_list[self.q_num].text} (True/False)?: ").lower()
-        # if self.q_list[self.q_num].answer.lower() == u_answer:
-        #     self.score += 1
-        #     self.q_num += 1
-        # print(self.score, self.q_num)
+        u_answer = input(f"Q.{self.q_num + 1}: {self.q_list[self.q_num].text} (True/False)?: ").lower()
+        if self.q_list[self.q_num].answer.lower() == u_answer:
+            self.score += 1
+            self.q_num += 1
+        print(self.score, self.q_num)
 
 # TODO: Create the class
 # TODO: asking the question
