6b311023757ae5f9132d2a2a09420a67d7ad9133
Pavneet <pavanghai@yahoo.com>
2024-07-20 00:08:29 -0700

    New Changes

---
+++ python/ReLearning/AngelaYuWithGurpreet/001-2_inputStatements.py
@@ -0,0 +1,10 @@
+print(input("What is your name?")) #❌
+print(input()) #✅
+
+# TYPE CASTING: default is string and we are converting it to int
+num1 = int(input())  
+num2 = int(input())
+
+print(num1 * num2)
+# Print length of the string that is provided by the user
+print(len(input()))
