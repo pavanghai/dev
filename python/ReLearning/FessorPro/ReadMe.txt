3c9b4c8dfbf78e9c850b84973dfb2b3a39e45295
Pavneet <pavanghai@yahoo.com>
2024-10-06 09:13:41 -0700

    Added Items in gitignore

--- python/ReLearning/FessorPro/ReadMe.txt
+++ python/ReLearning/FessorPro/ReadMe.txt
@@ -1,2 +1,5 @@
 Class codes 
   https://github.com/sunilsala88/batch-28
+  https://fessorpro.com/blogs/b/python-problems-on-datastructure
+  https://jsoncrack.com/editor
+  https://www.producthunt.com/products/json-crack
--- python/ReLearning/FessorPro/datastructure.py
+++ python/ReLearning/FessorPro/datastructure.py
@@ -62,3 +62,41 @@ print('string replace with new char', a)
 
 st5='    fsdfsd   '
 print('string strip', st5.strip())
+
+hedge_fund_portfolio = {
+    "fund_name": "Alpha Investments",
+    "portfolio_value": 50000000,
+    "investments": [
+
+        {
+            "type": "Equity",
+            "holdings": [
+                {"ticker": "AAPL", "quantity": 10000, "average_buy_price": 120},
+                {"ticker": "TSLA", "quantity": 5000, "average_buy_price": 600}
+            ]
+        },
+
+        {
+            "type": "Fixed Income",
+            "holdings": [
+                {"bond_issue": "US Treasuries", "amount": 10000000, "yield": 1.5}
+            ]
+        },
+
+        {
+            "type": "Derivatives",
+            "holdings": [
+                {"instrument": "Options", "details": {"underlying": "GOOGL", "type": "Call", "strike_price": 1500}}
+            ]
+        }
+
+    ],
+    "performance_metrics": {
+        "year_to_date_return": 5.2,
+        "five_year_annualized_return": 7.1
+    }
+}
+print(
+    hedge_fund_portfolio.get('investments', 'Not Found')[2]
+    .get('holdings', 'Not Found')[0].get('details', 'Not Found').get('strike_price', 'Not Found')
+)
